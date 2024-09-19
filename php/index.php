c0ebcc2f3bba7c8485a5639abe05ce75846561a1
Jeff White <Jeff.White@EnvisionHealth.com>
2024-09-19 10:59:57 -0400

    clean up

--- php/index.php
+++ php/index.php
@@ -3,13 +3,9 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Prometheus\CollectorRegistry;
-// use Prometheus\Storage\InMemory;
 use Prometheus\Storage\Redis as PrometheusRedis;
 use Prometheus\RenderTextFormat;
 use Storefront\Controllers\ProductController;
-
-// $redis = new Redis();
-// $redis->connect('sample-php-app-redis-master', 6379); // Redis service from Helm
 
 // Configure Redis connection for Prometheus metrics
 $redisAdapter = new PrometheusRedis([
@@ -19,12 +15,7 @@ $redisAdapter = new PrometheusRedis([
 
 // Initialize the registry with Redis storage
 $registry = new CollectorRegistry($redisAdapter);
-// $registry = new CollectorRegistry(new InMemory());
 
-// Initialize the counter if it doesn't exist
-// if (!$redis->exists('php_app_requests_total')) {
-//     $redis->set('php_app_requests_total', 0);
-// }
 
 // Metrics initialization
 $totalRequestsCounter = $registry->getOrRegisterCounter('php_app', 'requests_total', 'Total number of requests');
@@ -38,30 +29,21 @@ $totalRequestsCounter->inc();
 
 // Measure request duration with a histogram
 $memoryUsageGauge->set(memory_get_usage());
-$startTime = microtime(true);
-
-// Your application logic here
-
-$endTime = microtime(true);
-$requestDuration = $endTime - $startTime;
-$requestDurationHistogram->observe($requestDuration);
-
-// Increment the counter
-// $redis->incr('php_app_requests_total');
 
 // Expose metrics at /metrics
 if ($_SERVER['REQUEST_URI'] === '/metrics') {
   $renderer = new RenderTextFormat();
   header('Content-Type: ' . RenderTextFormat::MIME_TYPE);
-
-  // $requests_total = $redis->get('php_app_requests_total');
-  // echo "# HELP php_app_requests_total Total number of requests\n";
-  // echo "# TYPE php_app_requests_total counter\n";
-  // echo "php_app_requests_total " . $requests_total . "\n";
   echo $renderer->render($registry->getMetricFamilySamples());
   exit();
 } else {
+  $startTime = microtime(true);
+  
   // Create a ProductController instance
   $productController = new ProductController();
   $productController->showProduct();
+
+  $endTime = microtime(true);
+  $requestDuration = $endTime - $startTime;
+  $requestDurationHistogram->observe($requestDuration);
 }
